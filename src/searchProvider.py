6dc28e246cf248841f958d5832c16d3309056222
Sid Faber <sid.faber@canonical.com>
2021-08-24 16:52:23 -0400

    Minimum viable product (#1)
    * Initial MVP with a single search engine
    
    Signed-off-by: Sid Faber <sid@fabersl.com>

---
+++ src/searchProvider.py
@@ -0,0 +1,43 @@
+import ipaddress
+
+def normalizeIP (ip_str):
+    '''
+    Return a normalized dotted-quad version of an IP address
+    from a string. Return None if the string is not parseable
+    as an IP address.
+    '''
+    try:
+        ip_obj = ipaddress.IPv4Address (ip_str)
+    except ipaddress.AddressValueError:
+        return None
+    return str(ip_obj)
+
+# TODO: make name, title, homepage_url read-only properties that
+#   are required when instantiating the object
+# TODO: Move most of the javascript/html out of the template and into here
+#   as generator functions.
+# TODO: Rewrite the html_string functionality as a code generator
+class SearchProvider (object):
+    def __init__(self, name, title):
+        self.name = name
+        self.title = title
+        self.homepage_url = ""
+        self.html_string = "&nbsp;"
+
+    def json_url(self, q):
+        raise NotImplemented ("Method json_url is not implemented")
+        
+
+
+class IpapiSearch (SearchProvider):
+    def __init__(self):
+        super().__init__("ipapi", "IPApi")
+        self.homepage_url = "https://ipapi.co/"
+        self.html_string = '"<b>"+json.ip+"</b>: ASN "+json.asn+"; City "+json.city+"; Region "+json.region+"; Country "+json.country'
+    
+    def json_url (self, q):
+        addr = normalizeIP(q)
+        if addr is None:
+            return None
+        else:
+            return "https://ipapi.co/" + addr + "/json/"
---
+++ src/searchServer.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+#  Start with 
+# export FLASK_APP=api.py
+# flask run --host=0.0.0.0
+#
+# This flask project includes code from 
+# https://github.com/miguelgrinberg/flask-video-streaming
+# for setting up a streaming camera
+#
+from flask import Flask, render_template, Response, make_response
+from flask import request
+import logging
+
+from searchProvider import *
+
+import sys
+import time
+
+
+
+#logging.basicConfig(level=logging.DEBUG,
+#                            format='(%(threadName)-9s) %(message)s',)
+app = Flask(__name__)
+
+searchProviders = {}
+
+@app.route('/')
+@app.route('/search')
+def homepage():
+    return render_template ('search.html', searchProviders=searchProviders)
+
+
+def addSearchProvider (provider):
+    if provider.name in searchProviders.keys():
+        # only allow a single search provider of each type
+        raise KeyError ("Search provider %s has already been added" % (provider.name))
+    
+    searchProviders[provider.name] = provider
+    logging.debug ("Registered search provider " + provider.name)
+    return True
+
+if __name__ == '__main__':
+
+    addSearchProvider (IpapiSearch())
+    #app.run (host='0.0.0.0', port=80)
+    #app.run (host='0.0.0.0', port=5000)
+    app.run (port=5000)
+
+    logging.debug ("Shutting down")
+
