7229dac23d824f796051e3683d185af49e452294
Sid Faber <sid.faber@canonical.com>
2021-09-01 16:34:13 -0400

    Fix flake8 error (#4)
    Signed-off-by: Sid Faber <sid@fabersl.com>

--- src/searchServer.py
+++ src/searchServer.py
@@ -1,51 +1,44 @@
 #!/usr/bin/env python
 #
-#  Start with 
-# export FLASK_APP=api.py
-# flask run --host=0.0.0.0
+# Start with:
+#  export FLASK_APP=api.py
+#  flask run --host=0.0.0.0
 #
-# This flask project includes code from 
+# This flask project includes code from:
 # https://github.com/miguelgrinberg/flask-video-streaming
 # for setting up a streaming camera
 #
-from flask import Flask, render_template, Response, make_response
-from flask import request
+from flask import Flask, render_template
 import logging
-
-from searchProvider import *
-
-import sys
-import time
+from searchProvider import IpapiSearch
 
 
-
-#logging.basicConfig(level=logging.DEBUG,
-#                            format='(%(threadName)-9s) %(message)s',)
 app = Flask(__name__)
-
 searchProviders = {}
+
 
 @app.route('/')
 @app.route('/search')
 def homepage():
-    return render_template ('search.html', searchProviders=searchProviders)
+    return render_template('search.html', searchProviders=searchProviders)
 
 
-def addSearchProvider (provider):
+def addSearchProvider(provider):
     if provider.name in searchProviders.keys():
         # only allow a single search provider of each type
-        raise KeyError ("Search provider %s has already been added" % (provider.name))
-    
+        raise KeyError("Search provider %s has already been added"
+                       % (provider.name))
+
     searchProviders[provider.name] = provider
-    logging.debug ("Registered search provider " + provider.name)
+    logging.debug("Registered search provider " + provider.name)
     return True
+
 
 if __name__ == '__main__':
 
-    addSearchProvider (IpapiSearch())
-    #app.run (host='0.0.0.0', port=80)
-    #app.run (host='0.0.0.0', port=5000)
-    app.run (port=5000)
+    addSearchProvider(IpapiSearch())
+    # app.run(host='0.0.0.0', port=80)
+    # app.run(host='0.0.0.0', port=5000)
+    app.run(port=5000)
 
-    logging.debug ("Shutting down")
-
+    logging.debug("Shutting down")
